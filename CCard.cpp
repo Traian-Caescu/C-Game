2f0fa11504708d57df906cd9f291a4d8df0372bb
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-12-11 17:29:07 +0000

    Update CCard.cpp

--- CCard.cpp
+++ CCard.cpp
@@ -1,64 +1,36 @@
 #include "CCard.h"
-#include "CPlayer.h"
 
-// Constructor that reads data from a file stream and initializes the card's properties
-CCard::CCard(istream& file)
-{ 
-    file >> mName >> mSecondName >> mPower;  // Read card name, second name, and power from file
-    mName += (" " + mSecondName);  // Combine name and second name into a single string
+CCard::CCard(const std::string& cardName, const std::string& cardSecondName, int cardPower, int cardResilience)
+    : name(cardName), secondName(cardSecondName), power(cardPower), resilience(cardResilience) {}
+
+void CCard::PlayCard(std::shared_ptr<CPlayer> player, std::shared_ptr<CPlayer> enemy) {
+    std::cout << player->GetName() << " plays " << GetCardName() << "." << std::endl;
 }
 
-// Function that is called when the card is played by a player
-void CCard::PlayCard(shared_ptr<CPlayer> player, shared_ptr<CPlayer> enemy)
-{
-    cout << player->GetName() << " plays " << this->mName << endl;  // Display message indicating that the card has been played
-    player->DeleteCard(shared_from_this());  // Remove the card from the player's hand
+void CCard::AttackEnemy(std::shared_ptr<CPlayer> player, std::shared_ptr<CPlayer> enemy) {
+    std::cout << GetCardName() << " attacks " << enemy->GetName() << "." << std::endl;
+    enemy->SetPrestige(enemy->GetPrestige() - power);
+    std::cout << enemy->GetName() << "'s prestige is now " << enemy->GetPrestige() << "." << std::endl;
 }
 
-// Getter function that returns the name of the card
-string CCard::GetCardName()
-{
-    return mName;
+std::string CCard::GetCardName() const {
+    return name + " " + secondName;
 }
 
-// Getter function that returns the power of the card
-int CCard::GetCardPower()
-{
-    return mPower;
+int CCard::GetCardPower() const {
+    return power;
 }
 
-// Getter function that returns the resilience of the card
-int CCard::GetCardResilience() const
-{
-    return 0;
+int CCard::GetCardResilience() const {
+    return resilience;
 }
 
-// Overloaded input stream operator that reads card data from a file stream
-istream& operator>> (istream& file, CCard& card)
-{
-    file >> card.mName >> card.mSecondName >> card.mPower;  // Read card name, second name, and power from file
-    card.mName += (" " + card.mSecondName);  // Combine name and second name into a single string
-    return file;  // Return the input stream
+void CCard::SetCardPower(int newPower) {
+    power = newPower;
 }
 
-// Function that is called when the card attacks an enemy
-void CCard::AttackEnemy(shared_ptr<CPlayer> player, shared_ptr<CPlayer> enemy)
-{
+void CCard::SetCardResilience(int newResilience) {
+    resilience = newResilience;
 }
 
-// Setter function that sets the resilience of the card
-void CCard::SetCardResilience(int resilience)
-{
-}
-
-// Setter function that sets the power of the card
-void CCard::SetCardPower(int power)
-{
-    mPower = power;
-}
-
-
-
-// Destructor for the card class
-CCard::~CCard()
-= default;  // Use default implementation
+CCard::~CCard() = default;
